f102539828853760ee187e08e5eb4874faff4491
KingDome24 <git-kingdome24@simplebroadcast.net>
2014-11-02 15:26:50 +0100

    Changed order of loading configs and fixed indentation.

--- src/net/simplebroadcast/Main.java
+++ src/net/simplebroadcast/Main.java
@@ -22,7 +22,7 @@ import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
-	
+
 	public static Main plugin;
 	public static int running = 1;
 	public static int messageTask;
@@ -30,22 +30,34 @@ public class Main extends JavaPlugin {
 	private BossBarMethods bmt = new BossBarMethods();
 	private UpdatingMethods um = new UpdatingMethods();
 	public static MultiMap<Integer, String, String> globalMessages = new MultiMap<Integer, String, String>();
-	
+
 	@Override
 	public void onDisable() {
 		Bukkit.getScheduler().cancelTask(messageTask);
 		Bukkit.getScheduler().cancelTask(BossBarMethods.barTask);
 	}
-	
+
 	@SuppressWarnings("deprecation")
 	@Override
 	public void onEnable() {
-		
+
 		plugin = this;
-		
+
+		/*
+		 * Saves all configs and the readme
+		 */
+		plugin.saveDefaultConfig();
+		final File file = new File("plugins/SimpleBroadcast", "ignore.yml");
+		if (!(file.exists())) {
+			plugin.saveResource("ignore.yml", false);
+		}
 		File bossbar = new File("plugins/SimpleBroadcast", "bossbar.yml");
 		FileConfiguration cfg_boss = YamlConfiguration.loadConfiguration(bossbar);
-		
+		if (!bossbar.exists()) {
+			plugin.saveResource("bossbar.yml", false);
+		}
+		plugin.saveResource("readme.txt", true);
+
 		/*
 		 * Checks if the plugin shall be enabled and logs it if not.
 		 */
@@ -54,12 +66,12 @@ public class Main extends JavaPlugin {
 			Bukkit.getScheduler().cancelTask(messageTask);
 			running = 3;
 		}
-	
+
 		/*
 		 * Initializes the main command.
 		 */
 		plugin.getCommand("simplebroadcast").setExecutor(new SimpleBroadcastCommand(plugin));
-				
+
 		/*
 		 * Connects to mcstats.org and sends data (see list below).
 		 * - Prefix
@@ -72,24 +84,24 @@ public class Main extends JavaPlugin {
 		 */
 		if (plugin.getConfig().getBoolean("enabled") && plugin.getConfig().getBoolean("pluginmetrics")) {
 			try {
-			    Metrics metrics = new Metrics(plugin);
-			    Graph enabledFeatures = metrics.createGraph("Enabled Features");
-			    if (plugin.getConfig().getBoolean("prefix.enabled")) {
-			    	enabledFeatures.addPlotter(new Metrics.Plotter("Prefix") {
-			    		@Override
-			            public int getValue() {
-			    			return 1;
-			            }
-			    	});
-			    }
-			    if (plugin.getConfig().getBoolean("suffix.enabled")) {
-			    	enabledFeatures.addPlotter(new Metrics.Plotter("Suffix") {
-			    		@Override
-			            public int getValue() {
-			    			return 1;
-			            }
-			    	});
-			    }
+				Metrics metrics = new Metrics(plugin);
+				Graph enabledFeatures = metrics.createGraph("Enabled Features");
+				if (plugin.getConfig().getBoolean("prefix.enabled")) {
+					enabledFeatures.addPlotter(new Metrics.Plotter("Prefix") {
+						@Override
+						public int getValue() {
+							return 1;
+						}
+					});
+				}
+				if (plugin.getConfig().getBoolean("suffix.enabled")) {
+					enabledFeatures.addPlotter(new Metrics.Plotter("Suffix") {
+						@Override
+						public int getValue() {
+							return 1;
+						}
+					});
+				}
 //			    if (plugin.getConfig().getBoolean("usepermissions")) {
 //			    	enabledFeatures.addPlotter(new Metrics.Plotter("Permission messages") {
 //			    		@Override
@@ -98,53 +110,53 @@ public class Main extends JavaPlugin {
 //			            }
 //			    	});
 //			    }
-			    if (plugin.getConfig().getBoolean("checkforupdates")) {
-			    	enabledFeatures.addPlotter(new Metrics.Plotter("Update checker") {
-			    		@Override
-			            public int getValue() {
-			    			return 1;
-			            }
-			    	});
-			    }
-			    if (plugin.getConfig().getBoolean("randomizemessages")) {
-			    	enabledFeatures.addPlotter(new Metrics.Plotter("Message randomizer") {
-			    		@Override
-			            public int getValue() {
-			    			return 1;
-			            }
-			    	});
-			    }
-			    if (plugin.getConfig().getBoolean("requiresonlineplayers")) {
-			    	enabledFeatures.addPlotter(new Metrics.Plotter("Onlineplayers required") {
-			    		@Override
-			            public int getValue() {
-			    			return 1;
-			            }
-			    	});
-			    }
-			    if (plugin.getConfig().getBoolean("sendmessagestoconsole")) {
-			    	enabledFeatures.addPlotter(new Metrics.Plotter("Console messages") {
-			    		@Override
-			            public int getValue() {
-			    			return 1;
-			            }
-			    	});
-			    }
-			    if (cfg_boss.getBoolean("enabled")) {
-			    	enabledFeatures.addPlotter(new Metrics.Plotter("Bossbar") {
-			    		@Override
-			            public int getValue() {
-			    			return 1;
-			            }
-			    	});
-			    }
+				if (plugin.getConfig().getBoolean("checkforupdates")) {
+					enabledFeatures.addPlotter(new Metrics.Plotter("Update checker") {
+						@Override
+						public int getValue() {
+							return 1;
+						}
+					});
+				}
+				if (plugin.getConfig().getBoolean("randomizemessages")) {
+					enabledFeatures.addPlotter(new Metrics.Plotter("Message randomizer") {
+						@Override
+						public int getValue() {
+							return 1;
+						}
+					});
+				}
+				if (plugin.getConfig().getBoolean("requiresonlineplayers")) {
+					enabledFeatures.addPlotter(new Metrics.Plotter("Onlineplayers required") {
+						@Override
+						public int getValue() {
+							return 1;
+						}
+					});
+				}
+				if (plugin.getConfig().getBoolean("sendmessagestoconsole")) {
+					enabledFeatures.addPlotter(new Metrics.Plotter("Console messages") {
+						@Override
+						public int getValue() {
+							return 1;
+						}
+					});
+				}
+				if (cfg_boss.getBoolean("enabled")) {
+					enabledFeatures.addPlotter(new Metrics.Plotter("Bossbar") {
+						@Override
+						public int getValue() {
+							return 1;
+						}
+					});
+				}
 
-			    metrics.start();
+				metrics.start();
 			} catch (IOException e) {
 				logW(e.getMessage());
 			}
 		}
-		
+
 		/*
 		 * Checks if updates are available.
 		 * (Asynchronous task)
@@ -157,37 +169,24 @@ public class Main extends JavaPlugin {
 				}
 			});
 		}
-		
+
 		/*
 		 * Registers the events.
-		 */		
+		 */
 		if (plugin.getConfig().getBoolean("enabled")) {
 			getServer().getPluginManager().registerEvents(new Events(), plugin);
-		}			
-		
-	    /*
-	     * Saves all configs and the readme
-	     */		
-		plugin.saveDefaultConfig();
-		final File file = new File("plugins/SimpleBroadcast", "ignore.yml");
-		if (!(file.exists())) {
-			plugin.saveResource("ignore.yml", false);
 		}
-		if (!bossbar.exists()) {
-			plugin.saveResource("bossbar.yml", false);
-		}
-		plugin.saveResource("readme.txt", true);
-		
+
 		/*
-		 * Checks if the boss bar is enabled and BarAPI is installed.		
+		 * Checks if the boss bar is enabled and BarAPI is installed.
 		 */
 		if (cfg_boss.getBoolean("enabled") && Bukkit.getPluginManager().isPluginEnabled("BarAPI")) {
-			log("BarAPI integration successfully enabled.");			
+			log("BarAPI integration successfully enabled.");
 		} else {
 			Bukkit.getScheduler().cancelTask(BossBarMethods.barTask);
 			BossBarMethods.bar_running = 3;
 		}
-		
+
 		/*
 		 * Starts the chat broadcast task.
 		 */
@@ -203,14 +202,14 @@ public class Main extends JavaPlugin {
 				}
 			}
 		}
-		
+
 		/*
 		 * Starts the boss bar broadcast task.
 		 */
 		if (cfg_boss.getBoolean("enabled") && BossBarMethods.bar_running != 0 && BossBarMethods.bar_running != 3) {
 			bmt.barBroadcast();
 		}
-		
+
 		/*
 		 * Converts the old ignore.yml to the new format if existing.
 		 * Converts the user names to UUID's (asynchronous task).
@@ -219,12 +218,12 @@ public class Main extends JavaPlugin {
 		final FileConfiguration cfg_old = YamlConfiguration.loadConfiguration(file);
 		if (!old.exists() && cfg_old.get("format") == null) {
 			file.renameTo(old);
-			
+
 			final FileConfiguration cfg_new = YamlConfiguration.loadConfiguration(file);
 			final List<String> ignorePlayers = cfg_new.getStringList("players");
-			
+
 			final FileConfiguration cfg_converted = YamlConfiguration.loadConfiguration(old);
-			
+
 			try {
 				cfg_new.save(file);
 			} catch (IOException e) {
@@ -241,17 +240,17 @@ public class Main extends JavaPlugin {
 						} catch (Exception e) {
 							logW("Couldn't convert ignore.yml.");
 						}
-						
+
 						for (Map.Entry<String, UUID> entry : response.entrySet()) {
-						    UUID uuid = entry.getValue();
-						    ignorePlayers.add(uuid.toString());
-						    cfg_new.set("players", ignorePlayers);
-						    cfg_new.options().header("Since v1.7 the plugin uses the UUID of the player. Please DO NOT edit this file!");
+							UUID uuid = entry.getValue();
+							ignorePlayers.add(uuid.toString());
+							cfg_new.set("players", ignorePlayers);
+							cfg_new.options().header("Since v1.7 the plugin uses the UUID of the player. Please DO NOT edit this file!");
 						}
 						cfg_new.addDefault("format", "uuid");
 						cfg_new.options().copyDefaults(true);
 						try {
-					    	cfg_new.save(file);
+							cfg_new.save(file);
 						} catch (IOException e) {
 							logW("Couldn't save new ignore.yml.");
 						}
@@ -260,7 +259,7 @@ public class Main extends JavaPlugin {
 			});
 		}
 	}
-	
+
 	/**
 	 * Loads all the messages into a globally available MultiMap.
 	 */
@@ -273,10 +272,10 @@ public class Main extends JavaPlugin {
 			}
 		}
 	}
-	
+
 	/**
 	 * Logs the message with the status: info
-	 * @param str The message which shall be logged. 
+	 * @param str The message which shall be logged.
 	 */
 	public void log(String str) {
 		getLogger().info(str);
@@ -284,7 +283,7 @@ public class Main extends JavaPlugin {
 
 	/**
 	 * Logs the message with the status: warning
-	 * @param str The message which shall be logged. 
+	 * @param str The message which shall be logged.
 	 */
 	public void logW(String str) {
 		getLogger().warning(str);
